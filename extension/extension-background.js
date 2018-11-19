058a81710d5b8d7cd4b8155c178eb5d961123be8
Sergey Malinin <sergmalinin@gmail.com>
2018-11-19 21:56:49 +1000

    Update extension code

--- extension/extension-background.js
+++ extension/extension-background.js
@@ -47,7 +47,7 @@
            console.log('Error occurred: '+err);
         });
 
-//--        WebExtension.tabs.insertCSS(tab.id, {"code": "@font-face{font-family:'FontAwesome' ;src:url('" + WebExtension.extension.getURL('/media/fonts/fontawesome-webfont.eot?v=4.7.0') + "');src:url('" + WebExtension.extension.getURL('/media/fonts/fontawesome-webfont.eot?#iefix&v=4.7.0') + "') format('embedded-opentype'),url('" + WebExtension.extension.getURL('/media/fonts/fontawesome-webfont.woff2?v=4.7.0') + "') format('woff2'),url('" + WebExtension.extension.getURL('/media/fonts/fontawesome-webfont.woff?v=4.7.0') + "') format('woff'),url('" + WebExtension.extension.getURL('/media/fonts/fontawesome-webfont.ttf?v=4.7.0') + "') format('truetype'),url('" + WebExtension.extension.getURL('/media/fonts/fontawesome-webfont.svg?v=4.7.0#fontawesomeregular') + "') format('svg'); }"});
+        WebExtension.tabs.insertCSS(tab.id, {"code": "@font-face{font-family:'FontAwesome' ;src:url('" + WebExtension.extension.getURL('/media/fonts/fontawesome-webfont.eot?v=4.7.0') + "');src:url('" + WebExtension.extension.getURL('/media/fonts/fontawesome-webfont.eot?#iefix&v=4.7.0') + "') format('embedded-opentype'),url('" + WebExtension.extension.getURL('/media/fonts/fontawesome-webfont.woff2?v=4.7.0') + "') format('woff2'),url('" + WebExtension.extension.getURL('/media/fonts/fontawesome-webfont.woff?v=4.7.0') + "') format('woff'),url('" + WebExtension.extension.getURL('/media/fonts/fontawesome-webfont.ttf?v=4.7.0') + "') format('truetype'),url('" + WebExtension.extension.getURL('/media/fonts/fontawesome-webfont.svg?v=4.7.0#fontawesomeregular') + "') format('svg'); }"});
       }
 
       function showDocumentMenu(tab)
